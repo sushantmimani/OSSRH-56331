38605b9270ed1aaa673d086eb8e7a821b911bba8
Sushant Mimani <smimani@godaddy.com>
2021-05-06 15:43:24 -0700

    all testT

--- go/HelloWorld/scripts/release_prod.sh
+++ go/HelloWorld/scripts/release_prod.sh
@@ -6,9 +6,9 @@ ARTIFACT_NAME=$(go mod edit -json | jq -r '.Module.Path')
 TAG=`echo go/${ARTIFACT_NAME}/v${BASE_VERSION}`
 
 RESULT=$(git tag -l ${TAG})
-if [[ "$RESULT" != ${TAG}  && ${BASE_VERSION} != "0.0.0" ]]; then
+if [[ "$RESULT" != ${TAG}  ]]; then
     # Create tag
     echo "Creating a new release tag"
     git tag -f ${TAG} ${CIRCLE_SHA1}
-    ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa_git; git push origin --tags'
+    git push origin --tags
 fi
