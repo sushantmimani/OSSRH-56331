8af82e9fde467ef68ad93e383227a0835ca6b3b9
Sushant Mimani <smimani@godaddy.com>
2021-05-13 14:26:44 -0700

    CI

--- csharp/HelloWorld/scripts/release_prod.sh
+++ csharp/HelloWorld/scripts/release_prod.sh
@@ -4,10 +4,9 @@ set -e
 BASE_VERSION=$(xmllint --xpath "//Project/PropertyGroup/Version/text()" Directory.Build.props)
 ARTIFACT_NAME=$(xmllint --xpath "//Project/PropertyGroup/Title/text()" HelloWorld.csproj)
 TAG=`echo csharp/${ARTIFACT_NAME}/v${BASE_VERSION}`
-
 RESULT=$(git tag -l ${TAG})
 if [[ "$RESULT" != ${TAG} ]]; then
-    dotnet pack -c Release --no-build
+    dotnet pack -c Release
     echo "Release prod artifact"
     find . -name *${BASE_VERSION}.nupkg  | xargs -L1 -I '{}' dotnet nuget push {} -k ${NUGET_KEY} -s ${NUGET_SOURCE}
 
